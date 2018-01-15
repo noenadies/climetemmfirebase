20d4c1bb404a417d73df6c06c291b339baad6a31
noenadies <33671023+noenadies@users.noreply.github.com>
2018-01-15 15:39:03 -0500

    Update clasboton.js

--- clasboton.js
+++ clasboton.js
@@ -852,8 +852,10 @@ this.floatano=parseFloat(this.ano)+this.demesanos+this.dediaano;
 this.anodiferen=-1*(2018-(this.floatano))/100;
 
   this.mostrartexto(this.text_f1,"anteseno");
-this.anotem =0.02;//6*Math.asinh(this.anodiferen)+Math.sin(this.anodiferen);
-  
+this.anotem =6*ln(this.anodiferenx+Math.pow(Math.pow(this.anodiferen,2)+1,1/2))+Math.sin(this.anodiferen);
+
+//6*Math.asinh(this.anodiferen)+Math.sin(this.anodiferen);
+  //6*ln(this.anodiferenx+Math.pow(Math.pow(this.anodiferen,2)+1,1/2))th.sin(this.anodiferen);
 this.tempalturamin=-0.00530303*parseFloat(this.altura)+25+this.anotem+(-31*Math.pow(parseFloat(this.humedad)/100-0.5,3));
 this.amplitud=(-1.75571661329287/1000000*(this.altura*this.altura)) +(5.2144783414/1000*(this.altura)) +
         5.5111523119+this.anotem/6;
